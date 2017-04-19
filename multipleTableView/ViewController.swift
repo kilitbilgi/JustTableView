a8f328142bca0d94955207fb5eeb37a7e638eb08
burakcolak <kilitbilgi@gmail.com>
2017-04-20 00:50:51 +0300

    Initial Commit

---
+++ multipleTableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  multipleTableView
+//
+//  Created by Burak Colak on 4/20/17.
+//  Copyright © 2017 cool. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
